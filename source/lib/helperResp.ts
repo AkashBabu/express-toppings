2f4500f9bd09db25380cee2cfb24f01e824f362e
akashbabu <001akashbabu@gmail.com>
2017-08-20 11:27:24 +0530

    HandleResult -> defaultResult

--- source/lib/helperResp.ts
+++ source/lib/helperResp.ts
@@ -1,10 +1,13 @@
 import * as sh_Logger from "logger-switch"
 import { IHTTPResp } from "./helper"
 
+export interface ICallback {
+    (err: Error, result: any): void
+}
 export interface IHelperResp {
     unauth(res: IHTTPResp, comment?: string): void;
     serverError(res: IHTTPResp, comment?: string): void;
-    handleResult(res: IHTTPResp, err: Error, result: any, type?: string): void;
+    handleResult(res: IHTTPResp, defaultResult: any): ICallback;
     success(res: IHTTPResp, data?: any): void;
     failed(res: IHTTPResp, data?: string): void;
     post(res: IHTTPResp, data?: any): void;
@@ -48,41 +51,26 @@ export class HelperResp implements IHelperResp {
     /**
      * Callback Handler
      * @param res Express Response object
+     * @param defaultResult Default data, if no result was passed to callback
      */
-    public handleResult(res: IHTTPResp): Function {
-        return (err: Error, result: any, type = "array"): void => {
-            type = type.toLowerCase();
+    public handleResult(res: IHTTPResp, defaultResult: any): ICallback {
+        return (err: Error, result: any): void => {
+            defaultResult = defaultResult || [];
 
             if (!err) {
                 res.status(200).send({
                     error: false,
-                    data: this.isUndefined(result) ? (type == "array" ? [] : {}) : result
+                    data: this.isUndefined(result) ? defaultResult : result
                 })
             } else {
                 this.logger.error("HELPER_RESP HandleResult Err:", err)
                 res.status(500).send({
                     error: true,
-                    data: type == "array" ? [] : {}
+                    data: defaultResult
                 })
             }
         }
     }
-    // public handleResult(res: IHTTPResp, err: Error, result: any, type = "array"): void {
-    //     type = type.toLowerCase();
-
-    //     if (!err) {
-    //         res.status(200).send({
-    //             error: false,
-    //             data: this.isUndefined(result) ? (type == "array" ? [] : {}) : result
-    //         })
-    //     } else {
-    //         this.logger.error("HELPER_RESP HandleResult Err:", err)
-    //         res.status(500).send({
-    //             error: true,
-    //             data: type == "array" ? [] : {}
-    //         })
-    //     }
-    // }
 
     /**
      * 200 handler
